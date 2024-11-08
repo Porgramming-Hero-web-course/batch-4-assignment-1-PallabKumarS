3b5a8fc849cac1d94caebb76e145e7ff29f8b309
PallabKumarS <pallabkumar26@gmail.com>
2024-11-08 18:44:11 +0600

    second task is done

--- src/2.ts
+++ src/2.ts
@@ -1,3 +1,13 @@
 {
-    
+  const removeDuplicates = (array: number[]): number[] => {
+    const newArray: number[] = [];
+    array.forEach((element) => {
+      if (newArray.indexOf(element) === -1) {
+        newArray.push(element);
+      }
+    });
+    return newArray;
+  };
+
+  console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
 }
