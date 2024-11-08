413e2a71d02cfd6e7d2f0d80d79048173c10d105
PallabKumarS <pallabkumar26@gmail.com>
2024-11-08 18:11:43 +0600

    first task done

--- src/1.ts
+++ src/1.ts
@@ -1,3 +1,12 @@
 {
-    
+  const sumArray = (array: number[]): number => {
+    let sum = 0;
+    array.forEach((element) => {
+      sum = sum + element;
+    });
+
+    return sum;
+  };
+
+  //   console.log(sumArray([1, 2, 3, 4, 5]));
 }
